1d3757f6c8c578731c2bc6c2005080654d4e09c1
mashagrigorian <mashagrigorian5@gmail.com>
2023-06-30 22:12:42 +0400

    updated js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://api.openweathermap.org/data';
-const API_KEY = 'fd48bdf8a8b87b3c140f17625f4e2d57';
-const API_VERSION = '2.5';
+const API_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+    const GEOCODE_API_URL = 'https://api.openweathermap.org/geo/1.0/direct';
+    const API_KEY = 'fd48bdf8a8b87b3c140f17625f4e2d57';
 
 export {
-    API_URL, API_KEY, API_VERSION
+    API_URL, API_KEY, GEOCODE_API_URL
 }
